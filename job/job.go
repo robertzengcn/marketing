65af58bfc04f2be6328c53c687739527d3d8b0d2
jianze_zeng <jianze_zeng@foxitsoftware.com>
2023-03-04 16:57:00 +0800

    update tasks, add new task job

--- job/job.go
+++ job/job.go
@@ -10,7 +10,7 @@ func InitTask(){
 
 	cvtk1 := task.NewTask("tk1", "0 0 2 * * *", func(ctx context.Context) error { 
 		
-		logs.Info("run task per day")
+		logs.Info("run task at 2 per day")
 		scheduleModel:=models.Schedule{}
 		schVar, schErr:=scheduleModel.Findonebycyc("d")
 		if(schErr!=nil){
@@ -32,6 +32,31 @@ func InitTask(){
 	 })
 	task.AddTask("tk1", cvtk1)
 
+
+	cvtk2 := task.NewTask("tk1", "0 0 4 * * *", func(ctx context.Context) error { 
+		
+		logs.Info("run task at 4 per day")
+		scheduleModel:=models.Schedule{}
+		schVar, schErr:=scheduleModel.Findonebycyc("e")
+		if(schErr!=nil){
+			logs.Error(schErr)	
+			return schErr
+		}
+		staId,staerr:=scheduleModel.Createtask(schVar.Id)
+		if(staerr!=nil){
+			logs.Error(staerr)	
+			return staerr
+		}
+		logs.Info("task create with id"+strconv.FormatInt(staId, 10))
+		TaskModel := models.Task{}
+
+		go TaskModel.Starttask(staId)
+		// TaskModel.Updatetaskstatus(staId, 3)
+		return nil
+		
+	 })
+	task.AddTask("tk2", cvtk2)
+
 	// cvtk2 := task.NewTask("tk2", "0 0 1 * * *", func(ctx context.Context) error { 
 	// 	logs.Info("start to get adult keywords")
 	// 	KeywordModel:=models.Keyword{}
@@ -39,11 +64,11 @@ func InitTask(){
 	// })
 	// task.AddTask("tk2", cvtk2)
 	//get sex toy keyword
-	cvtk3 := task.NewTask("tk3", "0 30 1 * * *", func(ctx context.Context) error { 
-		logs.Info("start to get sex toy keywords")
-		KeywordModel:=models.Keyword{}
-		_,kerr:=KeywordModel.Getkeywordapi()
-		return kerr
-	})
-	task.AddTask("tk3", cvtk3)
+	// cvtk3 := task.NewTask("tk3", "0 30 1 * * *", func(ctx context.Context) error { 
+	// 	logs.Info("start to get sex toy keywords")
+	// 	KeywordModel:=models.Keyword{}
+	// 	_,kerr:=KeywordModel.Getkeywordapi()
+	// 	return kerr
+	// })
+	// task.AddTask("tk3", cvtk3)
 }
